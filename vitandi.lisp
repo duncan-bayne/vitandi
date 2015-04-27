d76a0a7b5f9aca1b7bb4cf1ce0bcdf27df09df2f
Duncan Bayne <dhgbayne@fastmail.fm>
2015-04-27 21:40:14 +1000

    Add a very basic scene scheduler

--- vitandi.lisp
+++ vitandi.lisp
@@ -1,5 +1,7 @@
 (in-package #:vitandi)
 
+(defvar current-scene)
+
 (setq drakma:*header-stream* nil)
 (setq drakma:*text-content-types* (cons '("application" . "json")
                                         drakma:*text-content-types*))
@@ -30,7 +32,23 @@
 
 (defun set-all-lights-to-scene (scene-name duration-s)
   "Sets all LIFX light bulbs to a particular scene, over a specified duration in seconds"
+  (print (format t "set-all-lights-to-scene: setting to ~a over ~a seconds" scene-name duration-s))
   (drakma:http-request "https://api.lifx.com/v1beta1/lights/all/color"
                        :method :put
                        :content (lifx-parameters-from-scene scene-name duration-s)
                        :basic-authorization `(,lifx-token ,lifx-password)))
+
+(defun lifx-scene-from-current-time ()
+  (let ((hour (nth 2 (multiple-value-list (get-decoded-time)))))
+    (if (or (>= hour 22) (< hour 5))
+        :night
+        :warm-daylight)))
+
+(defun main-loop ()
+  (loop
+     (let ((desired-scene (lifx-scene-from-current-time)))
+       (if (not (eq current-scene desired-scene))
+           (progn
+             (set-all-lights-to-scene desired-scene 3600)
+             (setq current-scene desired-scene))))
+     (sleep 60)))
