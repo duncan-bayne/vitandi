30cffb0267e399aa846a7b311aa2385dc7995f31
Duncan Bayne <dhgbayne@fastmail.fm>
2015-04-29 07:50:46 +1000

    Reduce night light to 2% brightness

--- scenes.lisp
+++ scenes.lisp
@@ -2,5 +2,5 @@
 
 (defvar scenes
   '(:warm-daylight (:brightness 1.0 :hue 32.6134 :kelvin 2500 :saturation 0.0)
-    :night (:brightness 0.05 :hue 32.6134 :kelvin 2500 :saturation 0.0)))
+    :night (:brightness 0.02 :hue 32.6134 :kelvin 2500 :saturation 0.0)))
 
