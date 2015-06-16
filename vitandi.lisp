a39db80e85a89be1c9285c97a2fc3f05a0a66173
Duncan Bayne <dhgbayne@fastmail.fm>
2015-06-16 21:00:46 +1000

    Log an error when LIFX API call fails

--- vitandi.lisp
+++ vitandi.lisp
@@ -26,20 +26,29 @@
                                         " kelvin:" (write-to-string kelvin)
                                         " saturation:" (write-to-string saturation)))))
 
-(defun get-all-lights-status ()
-  "Gets the status of all LIFX light bulbs."
+(defun check-all-lights-status ()
+  "Gets the status of all LIFX light bulbs, and emails alerts if any are offline"
   (json:decode-json-from-string
    (drakma:http-request "https://api.lifx.com/v1beta1/lights/all"
                         :method :get
                         :basic-authorization `(,lifx-token ,lifx-password))))
 
+(defun offline-p (result)
+  "Returns T if a particular light is offline, NIL otherwise"
+  (equalp "offline" (cdr (assoc :STATUS result))))
+
+(defun send-alert (result)
+  "Alerts the user that a LIFX API operation has failed"
+  (format t "LIFX API request has failed: ~a~%" result))
+
 (defun set-all-lights-to-scene (scene-name duration-s)
   "Sets all LIFX light bulbs to a particular scene, over a specified duration in seconds"
-  (print (format t "set-all-lights-to-scene: setting to ~a over ~a seconds" scene-name duration-s))
-  (drakma:http-request "https://api.lifx.com/v1beta1/lights/all/color"
-                       :method :put
-                       :content (lifx-parameters-from-scene scene-name duration-s)
-                       :basic-authorization `(,lifx-token ,lifx-password)))
+  (format t "set-all-lights-to-scene: setting to ~a over ~a seconds~%" scene-name duration-s)
+  (json:decode-json-from-string
+   (drakma:http-request "https://api.lifx.com/v1beta1/lights/all/color"
+                        :method :put
+                        :content (lifx-parameters-from-scene scene-name duration-s)
+                        :basic-authorization `(,lifx-token ,lifx-password))))
 
 (defun lifx-scene-from-current-time ()
   (multiple-value-bind
@@ -59,7 +68,8 @@
   (loop
      (let ((desired-scene (lifx-scene-from-current-time)))
        (if (not (eq current-scene desired-scene))
-           (progn
-             (set-all-lights-to-scene desired-scene 3600)
-             (setq current-scene desired-scene))))
+           (let ((result (set-all-lights-to-scene desired-scene 3600)))
+             (if (some #'offline-p result)
+                 (send-alert result)
+                 (setq current-scene desired-scene)))))
      (sleep 60)))
