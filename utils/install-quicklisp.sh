505ce4cc5b4212ff639436ae90900d83ff31c417
Duncan Bayne <duncan@bayne.id.au>
2015-06-17 16:43:17 +1000

    Prevent QL download if we won't install it

--- utils/install-quicklisp.sh
+++ utils/install-quicklisp.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-curl -O http://beta.quicklisp.org/quicklisp.lisp
-if [ ! -d ~/quicklisp ]; then sbcl --load quicklisp.lisp --eval "(progn (quicklisp-quickstart:install)(quit))" ; fi
+set -e
+
+if [ ! -d ~/quicklisp ]; then
+    curl -O http://beta.quicklisp.org/quicklisp.lisp
+    sbcl --load quicklisp.lisp --eval "(progn (quicklisp-quickstart:install)(quit))"
+fi
